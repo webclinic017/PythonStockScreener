ede78ac056f574ec4c222fe2e84aa87fae70e420
PHILIPP KAINDL <philipp.kaindl@hotmail.com>
2021-03-02 22:09:36 +0100

    inital stuff on yfinance hist func

--- 00_code/price_data_pull_yfinance.py
+++ 00_code/price_data_pull_yfinance.py
@@ -2,15 +2,25 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+from datetime import date 
 
-def getticker(ticker = '', saving = ):
+def gethistoricalOHLC(ticker, saving = False, start_date = '2000-01-01'):
     ''' finds a ticker and its representative OHLC data from yahoo finance. 
     Returns a pandas dataframe'''
     
-    if ticker is empty:
+    if ticker is None:
         print("Ticker is empty!!!")
     
-    
+    # TODO #1 #create a checkup if that data is already in the database 
+
+    # instantiate the yf obj
+    tickerObj = yf.Ticker(ticker)
+
+    print(date.today())
+    # get historical data
+    tickerHistoricalData = tickerObj.history(start = start_date, end = '2021-02-02', interval = '1d')
+
+    return tickerHistoricalData
 
 
-    return df
+print(gethistoricalOHLC("MSFT"))
