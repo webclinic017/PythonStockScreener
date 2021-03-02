6bde3731708b6488d427c489af5dd8bf4f5624fc
PHILIPP KAINDL <philipp.kaindl@hotmail.com>
2021-03-02 21:48:49 +0100

    adding a bunch of empty files for coding

---
+++ 00_code/price_data_pull_yfinance.py
@@ -0,0 +1,16 @@
+# Import statements
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+def getticker(ticker = '', saving = ):
+    ''' finds a ticker and its representative OHLC data from yahoo finance. 
+    Returns a pandas dataframe'''
+    
+    if ticker is empty:
+        print("Ticker is empty!!!")
+    
+    
+
+
+    return df
