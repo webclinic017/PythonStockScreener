079bdec0c1fc852c0b05d6caf0746972b21eae03
PHILIPP KAINDL <philipp.kaindl@hotmail.com>
2021-03-07 01:05:19 +0100

    nightly commit

---
+++ 00_code/global_vars.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+dataPathToOHLC = Path(
+    r"C:\Users\phili\Google Drive\Code_for_bothComputers\PythonStockMarketEvalTool\PythonStockScreener\01_data\01_price_data\\"
+)
+
+
+pathToMasterDF = Path(
+    r"C:\Users\phili\Google Drive\Code_for_bothComputers\PythonStockMarketEvalTool\PythonStockScreener\01_data\01_price_data\masterDF.csv"
+)
--- 00_code/price_data_pull_yfinance.py
+++ 00_code/price_data_pull_yfinance.py
@@ -4,6 +4,9 @@ import pandas as pd
 import os
 import numpy as np
 from datetime import date
+from pathlib import Path
+
+from global_vars import dataPathToOHLC, pathToMasterDF
 
 
 def gethistoricalOHLC(ticker, start_date="2000-01-01", end_date=None):
@@ -21,7 +24,6 @@ def gethistoricalOHLC(ticker, start_date="2000-01-01", end_date=None):
     # instantiate the yf obj
     tickerObj = yf.Ticker(ticker)
 
-    print(date.today())
     # get historical data
     tickerHistoricalData = tickerObj.history(
         start=start_date, end=end_date, interval="1d"
@@ -30,12 +32,78 @@ def gethistoricalOHLC(ticker, start_date="2000-01-01", end_date=None):
     return tickerHistoricalData
 
 
-def saveHistStockData():
-    """saves a retrieved historical stock data from """
+def saveHistStockData(ticker, tickerDF):
+    """saves a retrieved historical stock data from yfinance and puts it on the drive in the feather format.
+    Updates the master dataframe of stored stock data and their location.
+
+    Parameters
+    ----------
+    ticker : pandas.DataFrame
+
+    return
+    ------
+    data_writen_flag : bool
+    """
+    
+    updateFlag = False
+    first_date_of_data = tickerDF.index.min()  # get first date of the data available
+    last_date_of_data = tickerDF.index.max()  # get last date of the data available
+    filepath = dataPathToOHLC.joinpath(
+        ticker
+    )  # save the filepath to the respective ticker
+    masterDF = pd.read_csv(pathToMasterDF)  # open the masterDF and update the
+
+    if ticker in masterDF.TICKER.values:
+       # check if the min and max are below or above the existing entry
+       if (first_date_of_data < masterDF.FIRST_DATE_OHLC) or (last_date_of_data > masterDF.LAST_DATE_OHLC):
+            
+            # load the dataframe from disk
+            diskDF = pd.read_feather(filepath)
+            # merge the dataframes
+            tickerDF.reset_index(inplace=True)
+            resultDF = pd.merge(tickerDF, diskDF, how='outer').drop_duplicates(subset='Date')
+
+            # sort the new dataframe so we are sure everything is in order
+            resultDF = resultDF.sort_values(by='Date').reset_index()
+            first_date_of_data = resultDF.Date.min()
+            last_date_of_data = resultDF.Date.max()
+            updateFlag = True
+        
+        else:
+            data_writen_flag = False
+        
+        return data_writen_flag
+
+    else:  
+        # reindex the tickerDF to be written to feather
+        resultDF = tickerDF.reset_index()
+
+
+    # create a new entry and append to the masterDF
+    newEntry = {
+        "TICKER": ticker,
+        "FIRST_DATE_OHLC": first_date_of_data,
+        "LAST_DATE_OHLC": last_date_of_data,
+        "FILEPATH": filepath,
+    }
+
+    newEntry = pd.DataFrame.from_dict(newEntry)
+    if updateFlag == True:
+        masterDF = masterDF.update(newEntry)
+    else: 
+        masterDF = masterDF.append(newEntry)
+
+    pprint(masterDF.head())
+    masterDF.to_csv(pathToMasterDF, index=False)
+
+    # save the ticker data to a feather file
+    resultDF.to_feather(filepath)
+
+    data_writen_flag = True
+    return data_writen_flag
 
 
 hist = gethistoricalOHLC("POAHY")
 
-print(hist.columns)
 
-print(date.today())
+# masterDF = pd.DataFrame(columns=['TICKER', 'FIRST_DATE_OHLC', 'LAST_DATE_OHLC', 'FILEPATH'])
