dbc7bd3dd2aaaa10e65d25b761d317f546dfff55
philippkaindl <philpp.kaindl@hotmail.com>
2021-03-21 13:07:31 +0100

    minor updates

--- code/global_vars.py
+++ code/global_vars.py
@@ -11,6 +11,11 @@ pathToMasterDF = Path(
     r"C:\Users\Philipp\Google Drive\Code_for_bothComputers\PythonStockMarketEvalTool\PythonStockScreener\01_data\01_price_data\masterDF.csv"
 )
 
+dataPathToTechIndicators = Path(
+    # r"C:\Users\phili\Google Drive\Code_for_bothComputers\PythonStockMarketEvalTool\PythonStockScreener\01_data\01_price_data\\"
+    r"C:\Users\Philipp\Google Drive\Code_for_bothComputers\PythonStockMarketEvalTool\PythonStockScreener\01_data\01_price_data\01_price_data_w_tech_indicators\\"
+)
+
 
 float16Cols = ["Open", "High", "Low", "Close", "Dividends", "Stock Splits"]
 float32Cols = ["Volume"]
